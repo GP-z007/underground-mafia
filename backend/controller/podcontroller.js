a62765c11e32536de4da5bf83efcce82bd3f4e08
GP-z007 <gokulprasath572007@gmail.com>
2025-12-04 02:09:41 +0530

    add update get pod =)

--- backend/controller/podcontroller.js
+++ backend/controller/podcontroller.js
@@ -10,9 +10,50 @@ export const createPods=async(req,res)=>{
     })
 
 }
-
-export const getAllPods=(req,res) =>{
+// Get all pods
+export const getAllPods=async(req,res) =>{
+    const pods = await Pod.find()
     res.status(200).json({
-        message:"All Pods"
+        success:true,
+        pods
+    })
+}
+// update pods
+export const updatePod=async(req,res)=>{
+    
+    const pod=await Pod.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true
+    })
+    
+    res.status(200).json({
+        success:true,
+        pod
+    })
+}
+
+// Delete Product
+
+export const deletePod=async(req,res)=>{
+    
+    const pod=await Pod.findByIdAndDelete(req.params.id)  
+    res.status(200).json({
+        success:true,
+        message:"pod deleted successfully"
+    })
+}
+
+// acessing single pod
+export const getSinglePod=async(req,res)=>{
+    let pod = await Pod.findById(req.params.id);
+    if(!pod){
+        return res.status(500).json({
+            sucess:false,
+            message:"Pod is not found"
+        })
+    }
+    res.status(200).json({
+        success:true,
+        pod
     })
 }
--- backend/routes/podroutes.js
+++ backend/routes/podroutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {  createPods, getAllPods } from '../controller/podcontroller.js';
+import {  createPods, deletePod, getAllPods, getSinglePod, updatePod } from '../controller/podcontroller.js';
 
 const router = express.Router();
 
 //Routes
 router.route("/pods").get(getAllPods).post(createPods)
+router.route("/pod/:id").put(updatePod).delete(deletePod).get(getSinglePod);
 
 
 export default router;
